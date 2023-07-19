9196b49d0bd7b8cb1d5adb053c0acc0fed5097f6
Didiawesome <shodmankulovdidi@gmail.com>
2023-07-19 11:07:12 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg'
 import './App.css'
 import { useState } from 'react'
 import TodoWrapper from './components/TodoWrapper'
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <TodoWrapper />
     </div>
   )
